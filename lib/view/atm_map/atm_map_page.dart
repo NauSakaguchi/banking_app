100d355c5f5a1da75a5af1431e58238adc17181f
Nau Sakaguchi <nau.sakaguchi@gmail.com>
2023-04-12 13:59:37 -0700

    impl google map page

--- lib/view/atm_map/atm_map_page.dart
+++ lib/view/atm_map/atm_map_page.dart
@@ -1,101 +1,95 @@
 import 'dart:async';
 
 import 'package:banking_app/main.dart';
-import 'package:banking_app/view/atm_map/state/atm_map_page_provider.dart';
-import 'package:banking_app/view/atm_map/state/atm_map_page_state.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class AtmMapPage extends ConsumerWidget {
-  const AtmMapPage({super.key});
+class AtmMapPage extends StatefulWidget {
+  const AtmMapPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final mapState = ref.watch(atmMapProvider);
-    final store = ref.read(atmMapProvider.notifier);
+  State<AtmMapPage> createState() => AtmMapPageState();
+}
 
-    return Scaffold(
-      body: mapState.when(
-        data: (state) {
-          logger.d('map state updated');
-          logger.d('map state: ${state.currentPosition}');
-          if (state.currentPosition == null) {
-            return mapWithoutLocation(context, state);
-          } else {
-            final lat = state.currentPosition!.latitude;
-            final lng = state.currentPosition!.longitude;
-            CameraPosition cameraPosition = CameraPosition(
-              target: LatLng(lat, lng),
-              zoom: 14.4746,
-            );
-            logger.d("camera pos: $cameraPosition");
-            return mapWithLocation(context, state, cameraPosition);
-          }
-        },
-        error: (error, stack) {
-          logger.e(error);
-          logger.e(stack);
-          return defaultMap(context);
-        },
-        loading: () {
-          logger.d('map state loading...');
-          return defaultMap(context);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () async {
-          logger.d("pushed google map floatingActionButton");
-          try {
-            final pos = await store.determinePosition();
-            store.goToCurrentPos(pos);
-          } catch (e) {
-            logger.e("Location Permission: $e");
-          }
-        },
-        label: const Text('Search'),
-        icon: const Icon(Icons.search),
-      ),
-    );
+class AtmMapPageState extends State<AtmMapPage> {
+  Position? currentPosition;
+  late GoogleMapController _controller;
+  late bool _loading;
+  late LatLng _initialPosition;
+
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
+    distanceFilter: 100,
+  );
+  late StreamSubscription<Position> positionStream;
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+
+    //位置情報が許可されていない時に許可をリクエストする
+    Future(() async {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        await Geolocator.requestPermission();
+      }
+    });
+
+    _getUserLocation();
   }
 
-  Widget mapWithLocation(BuildContext context, AtmMapPageState atmState,
-      CameraPosition currentPosition) {
-    Completer<GoogleMapController> _controller = atmState.controller!;
-    logger.d("mapWithLocation: $currentPosition");
-
-    return GoogleMap(
-      mapType: MapType.normal,
-      myLocationButtonEnabled: false,
-      initialCameraPosition: currentPosition,
-      onMapCreated: (controller) {
-        _controller.complete(controller);
-      },
-    );
+  void _getUserLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _initialPosition = LatLng(position.latitude, position.longitude);
+      _loading = false;
+      logger.d("position: ${position.latitude}, ${position.longitude}");
+    });
   }
 
-  Widget mapWithoutLocation(BuildContext context, AtmMapPageState atmState) {
-    const CameraPosition kGooglePlex = CameraPosition(
-      target: LatLng(37.42796133580664, -122.085749655962),
-      zoom: 14.4746,
-    );
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
-    return mapWithLocation(context, atmState, kGooglePlex);
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  Widget build(BuildContext context) {
+    if (_loading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      return GoogleMap(
+        mapType: MapType.normal,
+        myLocationEnabled: true,
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 14.4746,
+        ),
+        onMapCreated: (GoogleMapController controller) {
+          _controller = controller;
+        },
+      );
+    }
+    // floatingActionButton: FloatingActionButton.extended(
+    //   onPressed: _goToTheLake,
+    //   label: const Text('Search'),
+    //   icon: const Icon(Icons.search),
+    // ),
+    // );
   }
 
-  Widget defaultMap(BuildContext context) {
-    Completer<GoogleMapController> _controller = Completer();
-    const CameraPosition _kGooglePlex = CameraPosition(
-      target: LatLng(37.42796133580664, -122.085749655962),
-      zoom: 14.4746,
-    );
-
-    return GoogleMap(
-      mapType: MapType.normal,
-      initialCameraPosition: _kGooglePlex,
-      onMapCreated: (controller) {
-        _controller.complete(controller);
-      },
-    );
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = _controller;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
--- lib/view/top/top_page.dart
+++ lib/view/top/top_page.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:banking_app/constant/route/route_path.dart';
+import 'package:banking_app/constant/string/globar_string.dart';
+import 'package:banking_app/view/atm_map/atm_map_page.dart';
 import 'package:banking_app/view_model/user_provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -11,34 +13,74 @@ class TopPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final userState = ref.watch(userInfoProvider);
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
     return WillPopScope(
       onWillPop: () async => false,
-      child: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          bottomNavigationBar: Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            color: colorScheme.background,
+            height: 70,
+            child: TabBar(
+              indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50), // Creates border
+                  color: colorScheme.primary),
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(
+                    Icons.home,
+                    color: colorScheme.onBackground,
+                  ),
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.map,
+                    color: colorScheme.onBackground,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          appBar: AppBar(
+            leading: const SizedBox(),
+            title: const Text(
+              GlobalString.APP_NAME,
+            ),
+          ),
+          body: TabBarView(
+            physics: const NeverScrollableScrollPhysics(),
             children: [
-              const Padding(
-                padding: EdgeInsets.all(20),
-                child: Text("Hello, your Unique ID is"),
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text("Hello, your Unique ID is"),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Text("${userState.uid}"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        await FirebaseAuth.instance.signOut();
+                        Navigator.pop(context);
+                      },
+                      child: const Text("Sign Out"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        context.router.pushNamed(RoutePath.atmMapRouteLogin);
+                      },
+                      child: const Text("ATM MAP"),
+                    ),
+                  ],
+                ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: Text("${userState.uid}"),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut();
-                  Navigator.pop(context);
-                },
-                child: const Text("Sign Out"),
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  context.router.pushNamed(RoutePath.atmMapRouteLogin);
-                },
-                child: const Text("ATM MAP"),
-              ),
+              AtmMapPage(),
             ],
           ),
         ),
