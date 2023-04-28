737a1aede1d6a726bb7e94e20c3620ba1b375e99
NauSakaguchi <sakaguchinau@gmail.com>
2023-04-27 19:52:58 -0700

    define check deposit page state

---
+++ lib/view/check_deposit/state/check_deposit_provider.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'check_deposit_page_state.dart';
+
+part 'check_deposit_provider.g.dart';
+
+@riverpod
+class CheckDepositItems extends _$CheckDepositItems {
+  @override
+  CheckDepositPageState build() {
+    return const CheckDepositPageState();
+  }
+
+  void updateAccountNumber(String str) {
+    state = state.copyWith(accountNumber: str);
+  }
+
+  void updateCheckAmount(int amount) {
+    state = state.copyWith(checkAmount: amount);
+  }
+
+  void updateRoutingNumber(String str) {
+    state = state.copyWith(routingNumber: str);
+  }
+
+  void updateCheckDate(DateTime date) {
+    state = state.copyWith(checkDate: date);
+  }
+
+  void updateCheckFrontImage(File file) {
+    state = state.copyWith(checkFrontImage: file);
+  }
+
+  void removeCheckFrontImage() {
+    state = state.copyWith(checkFrontImage: null);
+  }
+
+  void updateCheckBackImage(File file) {
+    state = state.copyWith(checkBackImage: file);
+  }
+
+  void removeCheckBackImage() {
+    state = state.copyWith(checkBackImage: null);
+  }
+}
---
+++ lib/view/check_deposit/state/check_deposit_provider.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'check_deposit_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+// ignore_for_file: avoid_private_typedef_functions, non_constant_identifier_names, subtype_of_sealed_class, invalid_use_of_internal_member, unused_element, constant_identifier_names, unnecessary_raw_strings, library_private_types_in_public_api
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+String _$CheckDepositItemsHash() => r'cba3d45e407929f0a3c4b08f70fbfae6717f0919';
+
+/// See also [CheckDepositItems].
+final checkDepositItemsProvider =
+    AutoDisposeNotifierProvider<CheckDepositItems, CheckDepositPageState>(
+  CheckDepositItems.new,
+  name: r'checkDepositItemsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$CheckDepositItemsHash,
+);
+typedef CheckDepositItemsRef
+    = AutoDisposeNotifierProviderRef<CheckDepositPageState>;
+
+abstract class _$CheckDepositItems
+    extends AutoDisposeNotifier<CheckDepositPageState> {
+  @override
+  CheckDepositPageState build();
+}
