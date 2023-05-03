0f1b65c09e5aa03fd57ace3f55f0cdaadb92d954
NauSakaguchi <sakaguchinau@gmail.com>
2023-05-03 13:05:56 -0700

    setup user register page

--- lib/view/sign_up/register_user_page.dart
+++ lib/view/sign_up/register_user_page.dart
@@ -1,3 +1,5 @@
+import 'package:banking_app/main.dart';
+import 'package:csc_picker/csc_picker.dart';
 import 'package:flutter/material.dart';
 
 class RegisterUserPage extends StatelessWidget {
@@ -23,9 +25,11 @@ class RegisterUserForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
     return SingleChildScrollView(
       child: Padding(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(12),
         child: Column(
           children: [
             const SizedBox(height: 30),
@@ -77,6 +81,32 @@ class RegisterUserForm extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 30),
+            const Text(
+              "Enter your address",
+              style: TextStyle(fontSize: 20),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child:
+
+                  ///Adding CSC Picker Widget in app
+                  CustomCSCPicker(colorScheme: colorScheme),
+            ),
+            // enter street address and zip code
+            TextField(
+              onChanged: (value) {},
+              decoration: const InputDecoration(
+                hintText: "Street Address",
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              onChanged: (value) {},
+              decoration: const InputDecoration(
+                hintText: "Zip Code",
+              ),
+            ),
+            const SizedBox(height: 30),
             ElevatedButton(
               onPressed: () {},
               child: const Text("Sign Up"),
@@ -84,6 +114,95 @@ class RegisterUserForm extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class CustomCSCPicker extends StatelessWidget {
+  const CustomCSCPicker({
+    Key? key,
+    required this.colorScheme,
+  }) : super(key: key);
+
+  final ColorScheme colorScheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return CSCPicker(
+      ///Enable disable state dropdown [OPTIONAL PARAMETER]
+      showStates: true,
+
+      /// Enable disable city drop down [OPTIONAL PARAMETER]
+      showCities: true,
+
+      ///Enable (get flag with country name) / Disable (Disable flag) / ShowInDropdownOnly (display flag in dropdown only) [OPTIONAL PARAMETER]
+      flagState: CountryFlag.DISABLE,
+
+      ///Dropdown box decoration to style your dropdown selector [OPTIONAL PARAMETER] (USE with disabledDropdownDecoration)
+      dropdownDecoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          border: Border.all(color: Colors.grey.shade300, width: 1)),
+
+      ///Disabled Dropdown box decoration to style your dropdown selector [OPTIONAL PARAMETER]  (USE with disabled dropdownDecoration)
+      disabledDropdownDecoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          color: colorScheme.tertiary,
+          border: Border.all(color: Colors.grey.shade300, width: 1)),
+
+      ///placeholders for dropdown search field
+      countrySearchPlaceholder: "Country",
+      stateSearchPlaceholder: "State",
+      citySearchPlaceholder: "City",
+
+      ///labels for dropdown
+      countryDropdownLabel: "Country",
+      stateDropdownLabel: "State",
+      cityDropdownLabel: "City",
+
+      ///Default Country
+      defaultCountry: CscCountry.United_States,
+
+      ///Country Filter [OPTIONAL PARAMETER]
+      // countryFilter: [
+      //   CscCountry.United_States,
+      // ],
+
+      ///Disable country dropdown (Note: use it with default country)
+      disableCountry: true,
+
+      ///selected item style [OPTIONAL PARAMETER]
+      selectedItemStyle: const TextStyle(fontSize: 14),
+
+      ///DropdownDialog Heading style [OPTIONAL PARAMETER]
+      dropdownHeadingStyle:
+          const TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+
+      ///DropdownDialog Item style [OPTIONAL PARAMETER]
+      dropdownItemStyle: const TextStyle(fontSize: 14),
+
+      ///Dialog box radius [OPTIONAL PARAMETER]
+      dropdownDialogRadius: 10.0,
+
+      ///Search bar radius [OPTIONAL PARAMETER]
+      searchBarRadius: 10.0,
+
+      ///triggers once country selected in dropdown
+      onCountryChanged: (value) {
+        logger.d(value);
+      },
+
+      ///triggers once state selected in dropdown
+      onStateChanged: (value) {
+        logger.d(value);
+      },
+
+      ///triggers once city selected in dropdown
+      onCityChanged: (value) {
+        logger.d(value);
+      },
+
+      ///Show only specific countries using country filter
+      // countryFilter: ["United States", "Canada", "Mexico"],
     );
   }
 }
